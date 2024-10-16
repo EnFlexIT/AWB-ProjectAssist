7e5613f4e9421c2f07f7de1a4c55bb6fcc4a82ce
CDerksen <christian.derksen@uni-due.de>
2024-10-16 16:29:10 +0200

    Initial project commit with first exercise

---
+++ de.enflexit.awb.assist.baseCase/src/de/enflexit/awb/assist/baseCase/AwbAssist.java
@@ -0,0 +1,18 @@
+package de.enflexit.awb.assist.baseCase;
+
+public class AwbAssist {
+
+	// --- Exercise ---
+	/*
+	 * Das Projekt 'baseCase' kopieren und in ein neues Projekt / einen neuen Ordner erstellen. 
+	 * 
+	 * - aus dem Ordner 'baseCase' soll der [SymBundleName] werden (z.B. net.peak.agent.prosumerAgent)
+	 * - die Parameter ('SymBundleName' & 'BundleName' & Zielverzeichnis) sollen als Startargumente übergeben werden können
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi Omar");
+
+	}
+
+}
