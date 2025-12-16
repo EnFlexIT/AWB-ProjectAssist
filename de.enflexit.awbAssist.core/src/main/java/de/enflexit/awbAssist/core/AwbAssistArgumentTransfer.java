fa1f6c820e06a6395c752708ac0b8685e406b6c1
OmarBenChobba <OmarBenChobba@FlexTeam-OBC.fritz.box>
2025-12-16 11:52:09 +0100

    Added an example file to use AWB Assist as maven plugin. Added an intermediate class for transferring arguments in a right form to the main class

---
+++ de.enflexit.awbAssist.core/src/main/java/de/enflexit/awbAssist/core/AwbAssistArgumentTransfer.java
@@ -0,0 +1,46 @@
+package de.enflexit.awbAssist.core;
+
+import java.util.HashMap;
+
+public class AwbAssistArgumentTransfer {
+	
+	public static final String BLUEPRINT = "blueprint";
+	public static final String BUNDLE_NAME = "bundleName";
+	public static final String SYMBOLIC_BUNDLE_NAME = "symBunName";
+	public static final String TARGET_DIRECTORY = "targetDir";
+	public static final String TECHNICAL_SYSTEM_NAME = "technicalSystemName";
+	
+	/**
+	 * passes a Hashmap of arguments in a correct form to awb assist main method.
+	 *
+	 * @param arguments the arguments
+	 */
+	public static void passArguments (HashMap<String, String> arguments) {
+		
+		if (arguments == null || arguments.isEmpty()) {
+			throw new IllegalArgumentException("Arguments map is null or empty");
+	    }
+
+	    String[] args = new String[arguments.size() * 2];
+
+	    int index = 0;
+	    for (HashMap.Entry<String, String> entry : arguments.entrySet()) {
+
+	        String key = entry.getKey();
+	        String value = entry.getValue();
+	        
+	        if (key == null || value == null || value.isBlank()) {
+	        	throw new IllegalArgumentException( "Invalid argument detected. Key: " + key + ", Value: " + value );
+	        }
+
+	        args[index] = "-" + key.toLowerCase();
+	        index ++;
+	        args[index] = value;
+	        index++;
+	    }
+
+	    AwbAssist.main(args);
+		
+	}
+
+}
